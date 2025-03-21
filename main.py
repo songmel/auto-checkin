5d2d88a7b4d602295b4264bb006075db01d8cb02
songmel <shj3269@gmail.com>
2025-03-22 00:00:28 +0900

    infinite cycle + system tray io

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+'''
+SKKU Learning Factory Bambu Lab Auto Check-in System
+Version : 1.0
+Author : Hwanjin Song
+Date : 2025-03-21
+
+'''
+
+import time
+import sys
+from threading import Thread
+import pygetwindow as gw
 import gspread
 import datetime
-import sys
 from PyQt5 import QtWidgets, uic, QtCore
+
+from pystray import MenuItem as item
+import pystray
+from PIL import Image
+
+
+#TARGET_TITLE = "Send to Printer MicroSD card"
+TARGET_TITLE = "제목 없음 - 메모장"
+WINDOW_X_AXIS, WINDOW_Y_AXIS = 300, 300
+SERVICE_ACCOUNT = 'service_account.json'
+GSPREAD_TITLE = 'checkin test'
+SHEET_NAME = 'sheet_test'
+
+POLLING_RATE = 0.1
+
 
 class UserForm(QtWidgets.QDialog):
     def __init__(self, sheet):
@@ -19,8 +45,8 @@ class UserForm(QtWidgets.QDialog):
         self.printnum = "0"
         self.printtime = "0시간 0분"
 
-        # 창 팝업 위치 설정
-        self.move(300, 300)
+        # !!!창 팝업 위치 설정!!!
+        self.move(WINDOW_X_AXIS, WINDOW_Y_AXIS)
 
         # 창의 테두리 제거
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)  # 테두리 없는 창 만들기
@@ -38,6 +64,24 @@ class UserForm(QtWidgets.QDialog):
         self.h_input.setPlaceholderText("0")  # 출력 시 입력 필드
         self.m_input.setPlaceholderText("0")  # 출력 분분 입력 필드
 
+    def reset_data(self):
+        # User 정보
+        self.name = "Unknown"
+        self.studentid = "00000000"
+        self.department = "Unknown"
+        self.contact = "000-0000-0000"
+        self.printnum = "0"
+        self.printtime = "0시간 0분"
+
+        # textbox 정보
+        self.name_input.clear()  # 이름 입력 필드
+        self.studentid_input.clear()  # 학번번 입력 필드
+        self.department_input.clear()  # 학과 입력 필드
+        self.contact_input.clear()  # 전화번호 입력 필드
+        self.printnum_input.clear()  # 프린터번호 입력 필드
+        self.h_input.clear()  # 출력 시 입력 필드
+        self.m_input.clear()  # 출력 분분 입력 필드
+
     def ok_clicked(self):
         current_time = datetime.datetime.now()
         formatted_time = current_time.strftime("%Y-%m-%d")
@@ -53,18 +97,21 @@ class UserForm(QtWidgets.QDialog):
         QtWidgets.QMessageBox.information(self, "전송완료", "전송을 완료했습니다.")
         self.accept()  # 다이얼로그 닫기
 
+
     def cancel_clicked(self):
         QtWidgets.QMessageBox.warning(self, "취소", "입력을 취소했습니다.")
         self.reject()  # 다이얼로그 닫기
+
+
 
 
 class sheet_manager():
     def __init__(self):
         super().__init__()
 
-        self.gc = gspread.service_account(filename='service_account.json') # !!!사용계정 api json으로 설정할 것 !!!
-        self.sh = self.gc.open("checkin test") # !!!스프레드시트 제목으로 설정할 것!!!
-        self.wsh = self.sh.worksheet("sheet_test") # !!!작업시트 이름으로 설정할 것!!!
+        self.gc = gspread.service_account(filename=SERVICE_ACCOUNT) # !!!사용계정 api json으로 설정할 것 !!!
+        self.sh = self.gc.open(GSPREAD_TITLE) # !!!스프레드시트 제목으로 설정할 것!!!
+        self.wsh = self.sh.worksheet(SHEET_NAME) # !!!작업시트 이름으로 설정할 것!!!
 
     def get_item(self):
         print(self.wsh.get('A1'))
@@ -78,11 +125,56 @@ class sheet_manager():
         next_row = last_filled_row + 1  # 다음 빈 행
         self.wsh.update(f"A{next_row}:G{next_row}", [[current_time, name, studentid, department, contact, printnum, printttime]])  # A열에 값 추가
 
+system_life = True
+
+def quit_action(icon):
+    global system_life
+    icon.stop()
+    system_life = False
+
+def tray_icon():
+
+    image = Image.open("tray_logo.png")
+    menu = (item('quit', quit_action), item('quit', quit_action))
+
+    icon = pystray.Icon("name", image, "LF Auto Check-in", menu)
+    icon.run()
+    
+    
 
 if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
     sheet = sheet_manager()
     window = UserForm(sheet)
-    window.show()
-    sys.exit(app.exec_())
+
+    th1 = Thread(target=tray_icon, args=())
+    th1.start()
+
+    send_finding = True
+
+    while (system_life):
+        while (send_finding and system_life):
+            for title in gw.getAllTitles():
+                if (title == TARGET_TITLE):
+                    print("SEND WINDOW OPEN")
+                    window.reset_data()
+                    window.show()
+                    app.exec_() # forever loop!
+                    send_finding = False
+                else:
+                    continue
+            time.sleep(POLLING_RATE)
+
+        while (not send_finding and system_life):
+            for title in gw.getAllTitles():
+                if (title == TARGET_TITLE):
+                    send_finding = False
+                    break
+                else:
+                    send_finding = True
+                    continue
+            time.sleep(POLLING_RATE)
+    
+    th1.join()
+    sys.exit()
